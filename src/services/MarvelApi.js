9b5fbae75ce603b908cb5aebf960349c1f1d1b2c
Jean Flesch <jean.flesch93@gmail.com>
2021-02-01 19:48:46 -0300

    Criar classe para serviços da API

---
+++ src/services/MarvelApi.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+class MarvelAPI {
+    constructor() {
+        this.publicKey = '9f10519dd487fe90f4db3fece4c12da0'
+        this.baseURL = 'https://gateway.marvel.com:443/v1/public'
+        this.axiosInstance = axios.create({
+            baseURL: this.baseURL
+        })
+    }
+
+    getCharacters (params) {
+        return this.axiosInstance.get(`/characters?apikey=${this.publicKey}`).then((response) => {
+            return response.data
+        })
+    }  
+
+    getComics (title, callback) {
+        let titleSearch = (title) ? `titleStartsWith=${title}` : ''
+
+        return this.axiosInstance.get(`/comics?${titleSearch}&orderBy=-focDate&apikey=${this.publicKey}`).then((response) => {
+            return response.data
+        })
+    }  
+}
+
+export default MarvelAPI
