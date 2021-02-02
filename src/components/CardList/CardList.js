85fcb0bdc0e319f60b32b0a78767ca23f408c18c
Jean Flesch <jean.flesch93@gmail.com>
2021-02-02 15:20:18 -0300

    Tratar apresentação em falha de conexão

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,11 +1,30 @@
 import CardListCSS from './CardList.module.css'
 import ComicCard from '../ComicCard/ComicCard'
-import Modal from '../UI/Modal/Modal'
+import Loader from '../UI/Loader/Loader'
 
 const cardList = (props) => {
-    let comics = <p>No comicse were found</p>
+    let comics = <p>No comics were found</p>
 
-    if(props.comics) {
+    // Loading content
+    if(props.loading) {
+        comics = (
+            <div className={ CardListCSS.Loader } >
+                <p>Carregando...</p>
+                <Loader show />
+            </div>
+        )
+    }
+
+    // Network error
+    else if(props.comics === -1) {
+        comics = (
+            <div className={ CardListCSS.Loader } >
+                <p style={{ textAlign: 'center', width: '100%' }}>Erro ao acessar API da Marvel. Por favor, tente novamente.</p>
+            </div>
+        )
+    }
+    // Render fetched content
+    else if(props.comics) {
         comics = props.comics.map((comic) => {
             return <ComicCard
                 key={ comic.id }
