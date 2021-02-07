7bf8a9fe43d1ebe0d22468cfc63db62d50dd4a67
Jean Flesch <jean.flesch93@gmail.com>
2021-02-07 00:56:34 -0300

    Estender componente para apresentação de hqs selecionadas

--- src/components/SendEmailButton/SendEmailButton.js
+++ src/components/SendEmailButton/SendEmailButton.js
@@ -2,11 +2,17 @@ import SendEmailButtonCSS from './SendEmailButton.module.scss'
 
 import { Component, Fragment } from 'react'
 import Mailer from '../../utils/Mailer'
+import ModalMail from '../UI/ModalMail/ModalMail'
 import Backdrop from './../UI/Backdrop/Backdrop'
 
 class SendEmailButton extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     state = {
-        isModalOpen: false
+        isModalOpen: false,
+        isLoadingModal: false
     }
 
     clickHandler = () =>{
@@ -16,7 +22,7 @@ class SendEmailButton extends Component {
         })
     }
 
-    closeModal = () => {
+    closeModalHandler = () => {
         this.setState({ isModalOpen: false })
     }
 
@@ -24,8 +30,17 @@ class SendEmailButton extends Component {
         return (
             <Fragment>
                 <Backdrop visible={ this.state.isModalOpen } clicked={ this.closeModal } />
+
+                <ModalMail 
+                    visible={ this.state.isModalOpen }
+					removed={ this.props.removed.bind(this) }
+					closed={ this.closeModalHandler.bind(this) }
+					loading={ this.state.isLoadingModal }
+					selectedComics={ this.props.selectedComics }
+                />
+
                 <button className={ SendEmailButtonCSS.Button } onClick={ this.clickHandler }>
-                    <i className="fas fa-envelope"></i>
+                    <i className="fas fa-paper-plane"></i>
                 </button>
             </Fragment>
         )
